e349d89ba0bccad74334ab1ccfc6497c0878d428
ygor.costa <ygor.costa@liferay.com>
2016-11-07 19:44:08 -0300

    Adds STATE stuff

--- src/Input.js
+++ src/Input.js
@@ -16,11 +16,103 @@ Soy.register(Input, templates);
 
 Input.STATE = {
 	/**
+	 * Defines "autocomplete" html attribute [on/off]
+	 * @type {string}
+	 * @default 'on'
+	 */
+	autocomplete: {
+		validator: core.isString,
+		value: 'on'
+	},
+	/**
+	 * Defines which classes this input field should have
+	 * @type {string}
+	 */
+	classes: {
+		validator: core.isString
+	},
+	/**
+	 * Defines the index of the field
+	 * @type {number}
+	 */
+	fieldIndex: {
+		validator: core.isNumber
+	},
+	/**
+	 * Defines if the value will appear as password or not when it starts
+	 * It only works combined with isTogglePassword
 	 * @type {boolean}
 	 * @default false
 	 */
 	isShowing: {
+		validator: core.isBoolean,
 		value: false
+	},
+	/**
+	 * Defines if this field has a behavior to hide and show the value
+	 * @type {boolean}
+	 * @default false
+	 */
+	isTogglePassword: {
+		validator: core.isBoolean,
+		value: false
+	},
+	/**
+	 * Defines the maximum length for this field.
+	 * @type {number}
+	 */
+	maxLength: {
+		validator: core.isNumber
+	},
+	/**
+	 * Defines the function name to 'onchange' event
+	 * @type {string}
+	 */
+	onChange: {
+		validator: core.isString
+	},
+	/**
+	 * Defines the function name to 'oninput' event
+	 * @type {string}
+	 */
+	onInput: {
+		validator: core.isString
+	},
+	/**
+	 * Defines "placeholder" html attribute
+	 * @type {string}
+	 */
+	placeholder: {
+		validator: core.isString
+	},
+	/**
+	 * Defines "readonly" html attribute
+	 * @type {boolean}
+	 */
+	readonly: {
+		validator: core.isBoolean
+	},
+	/**
+	 * Defines which row this field belongs to
+	 * @type {string}
+	 */
+	rowIndex: {
+		validator: core.isString
+	},
+	/**
+	 * @type {string}
+	 * @default 'text'
+	 */
+	type: {
+		validator: core.isString,
+		value: 'text'
+	},
+	/**
+	 * Defines the current value
+	 * @type {string}
+	 */
+	value: {
+		validator: core.isString
 	}
 };
 
